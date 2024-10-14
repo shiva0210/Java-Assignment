7f0a2df7fc28aa8cc41903a3294009f44c5f501c
Shivam Zodge <shivamzodge1600@gmail.com>
2024-10-14 22:31:02 +0530

    Day A  B  C  D

---
+++ New folder/p2.java
@@ -0,0 +1,19 @@
+class Demo{
+	public static void main(String [] args){
+	 for(int i=1;i<=4;i++){
+		 int a=1;
+			for(int j=1;j<=i;j++){
+				System.out.print(a+ "  ");
+				a++; 
+		}
+				System.out.println();
+	   }
+
+	}
+
+     }
+
+/*1
+1  2
+1  2  3
+1  2  3  4*/
